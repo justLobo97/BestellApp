e7fd28a68b40e5b529feaf57f134e41bbdd789f6
wagnermichael97 <wagner.michael.kf@gmail.com>
2026-09-15 15:24:57 +0200

    array edded + init

---
+++ JS/array.js
@@ -0,0 +1,110 @@
+let menu = [
+    {
+        name: "Veggie mushroom black burger",
+        description: "Chicken, Mozzarella, Gorgonzola, Fontina, Parmigiano Reggiano",
+        price: 16.90,
+        category: "Burger",
+        categoryImage: "./assets/logo/burger-logo.png",
+        image: "./assets/img/black-burger.png",
+        alt: "Black-Mushroom-Burger-Image"
+    },
+    {
+        name: "All meat burger",
+        description: "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ sauce",
+        price: 15.90,
+        category: "Burger",
+        categoryImage: "./assets/logo/burger-logo.png",
+        image: "./assets/img/all-meat-burger.png",
+        alt: "All-meat-burger-Image"
+    },
+    {
+        name: "Beef red burger",
+        description: "Beef, Cheese, Tomatoes, Lettuce, Onion",
+        price: 14.90,
+        category: "Burger",
+        categoryImage: "./assets/logo/burger-logo.png",
+        image: "./assets/img/red-burger.png",
+        alt: "Beef-red-burger-Image"
+    },
+    {
+        name: "Big chicken burger",
+        description: "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
+        price: 15.90,
+        category: "Burger",
+        categoryImage: "./assets/logo/burger-logo.png",
+        image: "./assets/img/chicken-burger.png",
+        alt: "Chicken-Burger-Image"
+    },
+    {
+        name: "Pizza Margherita",
+        description: "Tomato Sauce, Mozzarella",
+        price: 11.90,
+        category: "Pizza",
+        categoryImage: "./assets/logo/Pizza 1.png",
+        image: "./assets/img/margerita-pizza.png",
+        alt: "Pizza-Margherita-Image"
+    },
+    {
+        name: "Pizza Chorizo",
+        description: "Tomato slices, Mozzarella, Chorizo",
+        price: 13.90,
+        category: "Pizza",
+        categoryImage: "./assets/logo/Pizza 1.png",
+        image: "./assets/img/chorizo-pizza.png",
+        alt: "Pizza-Chorizo-Image"
+    },
+    {
+        name: "Pizza Funghi",
+        description: "Red onion, Olives, Button Mushrooms, Mozzarella",
+        price: 12.90,
+        category: "Pizza",
+        categoryImage: "./assets/logo/Pizza 1.png",
+        image: "./assets/img/funghi-pizza.png",
+        alt: "Pizza-Funghi-Image"
+    },
+    {
+        name: "Quattro Formaggi with Chicken",
+        description: "Chicken, Mozzarella, Gorgonzola, Fontina, Parmigiano Reggiano",
+        price: 16.90,
+        category: "Pizza",
+        categoryImage: "./assets/logo/Pizza 1.png",
+        image: "./assets/img/formaggi-pizza.png",
+        alt: "Formaggi-Pizza-Image"
+    },
+    {
+        name: "Warm beef arugula salad",
+        description: "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
+        price: 16.90,
+        category: "Salad",
+        categoryImage: "./assets/logo/Salad 1.png",
+        image: "./assets/img/beef-salad.png",
+        alt: "Beef-Salad-Image"
+    },
+    {
+        name: "Mini green salad",
+        description: "Green salad, Cucumber, Carrots, Parsley, Radishes",
+        price: 7.90,
+        category: "Salad",
+        categoryImage: "./assets/logo/Salad 1.png",
+        image: "./assets/img/mini-green-salad.png",
+        alt: "Mini-Green-Salad-Image"
+    },
+    {
+        name: "Green salad with sea food",
+        description: "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+        price: 16.90,
+        category: "Salad",
+        categoryImage: "./assets/logo/Salad 1.png",
+        image: "./assets/img/green-salad.png",
+        alt: "Sea-Food-Salad-Image"
+    },
+    {
+        name: "Vegan green salad with tofu",
+        description: "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+        price: 14.90,
+        category: "Salad",
+        categoryImage: "./assets/logo/Salad 1.png",
+        image: "./assets/img/beef-salad.png",
+        alt: "Tofu-Salad-Image"
+    }
+];
---
+++ JS/templates.js
@@ -0,0 +1,27 @@
+
+let menuWrapper = document.getElementById('menu-wrapper');
+
+for (let i = 0; i < menu.length; i++) {
+
+    if (i === 0 || menu[i].category !== menu[i - 1].category) {
+    menuWrapper.innerHTML += `
+        <div class="menu-title">
+            <img class="menu-logo" src="${menu[i].categoryImage}" alt="${menu[i].category}-Logo">
+            <div class="menu-header">
+                <h2>${menu[i].category}</h2>
+            </div>
+        </div>`;
+    }
+
+
+    menuWrapper.innerHTML += `
+        <div class="menu-card">
+            <img src="${menu[i].image}" alt="${menu[i].alt}">
+            <h3>${menu[i].name}</h3>
+            <p>${menu[i].description}</p>
+                <div class="card-footer">
+                    <p>${menu[i].price.toFixed(2).replace('.', ',')}€</p>
+                    <button>Add to basket</button>
+                </div>
+        </div>`;
+}
