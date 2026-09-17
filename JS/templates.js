27481b4dcb1c7c7a502d8768f8d41caeb575134d
wagnermichael97 <wagner.michael.kf@gmail.com>
2026-09-17 14:15:44 +0200

    final fix

--- JS/templates.js
+++ JS/templates.js
@@ -6,9 +6,11 @@ for (let i = 0; i < menu.length; i++) {
     if (i === 0 || menu[i].category !== menu[i - 1].category) {
         menuWrapper.innerHTML += `
         <div class="menu-title">
-            <img class="menu-logo" src="${menu[i].categoryImage}" alt="${menu[i].category}-Logo">
-            <div class="menu-header">
-                <h2>${menu[i].category}</h2>
+           <div class="menu-title-content">
+               <img class="menu-logo" src="${menu[i].categoryImage}" alt="${menu[i].category}-Logo">
+                <div class="menu-header">
+             <h2>${menu[i].category}</h2>
+                </div>
             </div>
         </div>`;
     }
